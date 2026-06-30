4b4a5ebfedb591192387e373fe70e67e487520de
ajay <ajaymaurya892483@gmail.com>
2026-06-30 08:30:41 +0530

    Update production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,17 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:3000/api",
+//   withCredentials: true,
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://collegeproject-0yt9.onrender.com/api",
   withCredentials: true,
 });
 
